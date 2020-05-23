6c8fd03863c8aa95076d792573fd3c0f43bbffa5
rajan-blackboxes <63178037+rajan-blackboxes@users.noreply.github.com>
2020-05-23 12:27:37 +0545

    add final project

--- Project/serve/predict.py
+++ Project/serve/predict.py
@@ -70,9 +70,8 @@ def predict_fn(input_data, model):
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
 
-    data_X = None
-    data_len = None
-    data_X, data_len = convert_and_pad(build_dict(input_data, vocab_size = 500) ,review_to_words(input_data))
+
+    data_X, data_len = convert_and_pad(model.word_dict ,review_to_words(input_data))
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
